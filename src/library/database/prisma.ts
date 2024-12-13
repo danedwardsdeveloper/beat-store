208d2c2bf26f8d22e3046edd56b8f7950d0d3c9f
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-13 11:50:38 +0000

    Create prisma.ts

---
+++ src/library/database/prisma.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from '@prisma/client'
+
+import { isProduction } from '../environment'
+
+type GlobalWithPrisma = typeof globalThis & {
+  prisma: PrismaClient | undefined
+}
+
+declare const global: GlobalWithPrisma
+
+let prisma: PrismaClient
+
+if (isProduction) {
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  prisma = global.prisma
+}
+
+export default prisma
