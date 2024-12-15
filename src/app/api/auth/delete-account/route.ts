14ab83ab59b2a2c510e9f78bcbc5845dd1514b5e
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-15 18:27:09 +0000

    Update route.ts

--- src/app/api/auth/delete-account/route.ts
+++ src/app/api/auth/delete-account/route.ts
@@ -1,12 +1,55 @@
-// import prisma from "@/library/database/prisma"
-// import { NextResponse } from "next/server"
+import jwt from 'jsonwebtoken'
+import { NextRequest, NextResponse } from 'next/server'
+
+import { CookieName, createClearCookieOptions, jwtSecret, TokenPayload } from '@/library/cookies'
+import prisma from '@/library/database/prisma'
+import logger from '@/library/logger'
 
 export interface DeleteDeleteAccountResponse {
-  message: 'success' | 'server error'
+  message:
+    | 'success' //
+    | 'token missing'
+    | 'invalid token'
+    | 'user not found'
+    | 'db error'
+    | 'server error'
 }
 
-// export async function DELETE(): Promise <NextResponse<DeleteDeleteAccountResponse>> {
-//   try (
-//     await prisma
-//   )
-// }
+export async function DELETE(request: NextRequest): Promise<NextResponse<DeleteDeleteAccountResponse>> {
+  try {
+    const token = request.cookies.get('token' as CookieName)?.value
+
+    if (!token) {
+      logger.info('Token missing')
+      return NextResponse.json({ message: 'token missing' }, { status: 401 })
+    }
+
+    let decoded: TokenPayload
+    try {
+      decoded = jwt.verify(token, jwtSecret) as TokenPayload
+    } catch (error) {
+      logger.error('Invalid token error: ', error)
+      return NextResponse.json({ message: 'invalid token' }, { status: 401 })
+    }
+
+    try {
+      await prisma.user.delete({
+        where: { id: decoded.sub },
+      })
+    } catch (error) {
+      logger.error('Database error while trying to delete account: ', error)
+      return NextResponse.json({ message: 'db error' }, { status: 500 })
+    }
+
+    const response = NextResponse.json(
+      { message: 'success' } as DeleteDeleteAccountResponse, //
+      { status: 200 },
+    )
+    response.cookies.set('token' as CookieName, '', createClearCookieOptions())
+
+    return response
+  } catch (error) {
+    logger.error('Delete account error:', error)
+    return NextResponse.json({ message: 'server error' }, { status: 500 })
+  }
+}
