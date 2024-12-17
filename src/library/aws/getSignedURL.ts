350a27f895edf5214ced2319d4bf922d0e590b45
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-17 21:09:10 +0000

    Create getSignedURL.ts

---
+++ src/library/aws/getSignedURL.ts
@@ -0,0 +1,27 @@
+import { getSignedUrl } from '@aws-sdk/cloudfront-signer'
+
+import { cfDomain, cfKeyPairId, cfPrivateKey } from './configuration'
+// ToDo move this definition
+import { AssetType } from '@/app/api/admin/beats/[beatId]/assets/route'
+
+const thirtyMinutesInMilliseconds = 30 * 60 * 1000
+
+export type SignedAssetType = Exclude<AssetType, 'image' | 'untaggedMp3'>
+
+export function generateSignedURL(key: string, assetType: SignedAssetType) {
+  const fileType = {
+    taggedMp3: 'mp3',
+    wav: 'wav',
+    zippedStems: 'zip',
+  }[assetType]
+
+  const url = `https://${cfDomain}/${key}.${fileType}`
+  const dateLessThan = new Date(Date.now() + thirtyMinutesInMilliseconds).toISOString()
+
+  return getSignedUrl({
+    url,
+    keyPairId: cfKeyPairId,
+    privateKey: cfPrivateKey,
+    dateLessThan,
+  })
+}
