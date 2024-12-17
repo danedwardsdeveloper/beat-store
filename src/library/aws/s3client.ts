3d71a49f43db726f0ef9231f2182561401cbcfe4
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-17 21:09:18 +0000

    Create s3client.ts

---
+++ src/library/aws/s3client.ts
@@ -0,0 +1,11 @@
+import { S3Client } from '@aws-sdk/client-s3'
+
+import { awsAccessKey, awsSecretAccessKey } from './configuration'
+
+export const s3Client = new S3Client({
+  region: 'us-east-1',
+  credentials: {
+    accessKeyId: awsAccessKey,
+    secretAccessKey: awsSecretAccessKey,
+  },
+})
