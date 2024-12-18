ef96cdde9265cfda8fa6c61ac6f54687ed2d1af2
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-18 21:25:50 +0000

    Remove template content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,23 @@
-import type { Metadata } from 'next';
-import { Readex_Pro } from 'next/font/google';
-import './globals.tailwind.css';
+import type { Metadata } from 'next'
 
-const readexPro = Readex_Pro({
-	subsets: ['latin'],
-	variable: '--font-readex-pro',
-});
+import Menu from '@/components/menu'
 
 export const metadata: Metadata = {
-	title: 'Beat Store',
-	description: 'A modern e-commerce app for selling beats',
-};
+  title: 'Beat Store',
+  description: 'A modern e-commerce app for selling beats',
+}
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode
 }>) {
-	return (
-		<html lang="en-GB">
-			<body className={`${readexPro.variable} font-sans antialiased`}>
-				{children}
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en-GB">
+      <body className="font-sans antialiased">
+        <Menu />
+        {children}
+      </body>
+    </html>
+  )
 }
