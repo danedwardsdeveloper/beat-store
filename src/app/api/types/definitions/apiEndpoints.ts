fb9ad3c2d1b6d7f5d120ef0301451cde10422ee0
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-18 21:25:17 +0000

    Remove apiPaths

--- src/app/api/types/definitions/apiEndpoints.ts
+++ src/app/api/types/definitions/apiEndpoints.ts
@@ -1,7 +1,7 @@
 import { Beat } from '@prisma/client'
 
-import { AdminBeatsAssetsBodyPOST } from '@/app/api/admin/beats/[beatId]/assets/route'
-import { BeatsResponseDELETE, BeatsResponsePATCH } from '@/app/api/admin/beats/[beatId]/route'
+import { AdminBeatsAssetsBodyPOST } from '@/app/api/admin/beats/[beatIdentifier]/assets/route'
+import { BeatsResponseDELETE, BeatsResponsePATCH } from '@/app/api/admin/beats/[beatIdentifier]/route'
 import { AdminBeatsResponseGET, AdminBeatsResponsePOST } from '@/app/api/admin/beats/route'
 import { RegenerateResponsePOST } from '@/app/api/admin/regenerate/route'
 import { CreateAccountBodyPOST, CreateAccountResponsePOST } from '@/app/api/auth/create-account/route'
@@ -9,10 +9,6 @@ import { DeleteAccountResponseDELETE } from '@/app/api/auth/delete-account/route
 import { SignInBodyPOST, SignInResponsePOST } from '@/app/api/auth/sign-in/route'
 import { SignOutResponseGET } from '@/app/api/auth/sign-out/route'
 import { BeatsGET } from '@/app/api/beats/route'
-
-export const apiPaths = Object.freeze(
-  Object.fromEntries(Object.keys({} as ApiEndpoints).map(key => [key, key])),
-) as { [K in keyof ApiEndpoints]: K }
 
 export interface ApiEndpoints {
   '/api/auth/create-account': {
