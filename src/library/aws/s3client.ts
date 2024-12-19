f2cb7e6a8e0e900cfd4185b301d66827a7d4f368
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-19 11:00:39 +0000

    Update import path

--- src/library/aws/s3client.ts
+++ src/library/aws/s3client.ts
@@ -1,6 +1,6 @@
 import { S3Client } from '@aws-sdk/client-s3'
 
-import { awsAccessKey, awsSecretAccessKey } from './configuration'
+import { awsAccessKey, awsSecretAccessKey } from './awsConfiguration'
 
 export const s3Client = new S3Client({
   region: 'us-east-1',
