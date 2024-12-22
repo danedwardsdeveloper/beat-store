74ce334935b44a162230dc78925254f5a29d558f
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-22 10:03:54 +0000

    Simplify business logic

--- src/library/aws/getSignedURL.ts
+++ src/library/aws/getSignedURL.ts
@@ -1,27 +1,27 @@
-import { getSignedUrl } from '@aws-sdk/cloudfront-signer'
+// import { getSignedUrl } from '@aws-sdk/cloudfront-signer'
 
-import { cfDomain, cfKeyPairId, cfPrivateKey } from './awsConfiguration'
-// ToDo move this definition
-import { AssetType } from '@/app/api/admin/beats/[beatIdentifier]/assets/route'
+// import { cfDomain, cfKeyPairId, cfPrivateKey } from './awsConfiguration'
+// // ToDo move this definition
+// import { AssetType } from '@/app/api/admin/beats/[beatIdentifier]/assets/route'
 
-const thirtyMinutesInMilliseconds = 30 * 60 * 1000
+// const thirtyMinutesInMilliseconds = 30 * 60 * 1000
 
-export type SignedAssetType = Exclude<AssetType, 'image' | 'untaggedMp3'>
+// export type SignedAssetType = Exclude<AssetType, 'image' | 'untaggedMp3'>
 
-export function generateSignedURL(key: string, assetType: SignedAssetType) {
-  const fileType = {
-    taggedMp3: 'mp3',
-    wav: 'wav',
-    zippedStems: 'zip',
-  }[assetType]
+// export function generateSignedURL(key: string, assetType: SignedAssetType) {
+//   const fileType = {
+//     taggedMp3: 'mp3',
+//     wav: 'wav',
+//     zippedStems: 'zip',
+//   }[assetType]
 
-  const url = `https://${cfDomain}/${key}.${fileType}`
-  const dateLessThan = new Date(Date.now() + thirtyMinutesInMilliseconds).toISOString()
+//   const url = `https://${cfDomain}/${key}.${fileType}`
+//   const dateLessThan = new Date(Date.now() + thirtyMinutesInMilliseconds).toISOString()
 
-  return getSignedUrl({
-    url,
-    keyPairId: cfKeyPairId,
-    privateKey: cfPrivateKey,
-    dateLessThan,
-  })
-}
+//   return getSignedUrl({
+//     url,
+//     keyPairId: cfKeyPairId,
+//     privateKey: cfPrivateKey,
+//     dateLessThan,
+//   })
+// }
