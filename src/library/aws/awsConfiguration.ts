63517fafcb318fd70510be2d64af18b243281d2d
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-22 10:04:07 +0000

    Rename imported function

--- src/library/aws/awsConfiguration.ts
+++ src/library/aws/awsConfiguration.ts
@@ -1,8 +1,8 @@
-import { requireEnv } from '../environment'
+import { requireVariable } from '@/library/environment/requireVariable'
 
-export const awsAccessKey = requireEnv('AWS_ACCESS_KEY_ID')
-export const awsSecretAccessKey = requireEnv('AWS_SECRET_ACCESS_KEY')
+export const awsAccessKey = requireVariable('AWS_ACCESS_KEY_ID')
+export const awsSecretAccessKey = requireVariable('AWS_SECRET_ACCESS_KEY')
 
-export const cfKeyPairId = requireEnv('CF_KEY_PAIR_ID')
-export const cfPrivateKey = requireEnv('CF_PRIVATE_KEY')
-export const cfDomain = requireEnv('CF_DOMAIN')
+// export const cfKeyPairId = requireVariable('CF_KEY_PAIR_ID')
+// export const cfPrivateKey = requireVariable('CF_PRIVATE_KEY')
+// export const cfDomain = requireVariable('CF_DOMAIN')
