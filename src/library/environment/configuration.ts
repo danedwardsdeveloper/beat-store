79484404cfc955c59a54cdb2304d9d44db8b5137
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-23 19:51:44 +0000

    Add CloudFront url

--- src/library/environment/configuration.ts
+++ src/library/environment/configuration.ts
@@ -3,6 +3,11 @@ export const isDevelopment = process.env.NODE_ENV === 'development'
 
 export const bareFlyDomain = 'beat-store.fly.dev'
 export const bareCustomDomain = 'beatstore.co.uk'
+
 export const productionBaseURL = `https://${bareCustomDomain}`
 export const developmentBaseURL = 'http://localhost:3000'
 export const dynamicBaseURL = isProduction ? productionBaseURL : developmentBaseURL
+
+const productionAssetsBaseURL = `https://assets.${bareCustomDomain}`
+const developmentAssetsBaseURL = 'https://d1ttulk89fbidl.cloudfront.net'
+export const dynamicAssetsBaseURL = isProduction ? productionAssetsBaseURL : developmentAssetsBaseURL
