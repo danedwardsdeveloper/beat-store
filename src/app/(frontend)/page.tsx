cfad31f8244f018be9ae2019831e55d01964c53b
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-27 12:41:26 +0000

    Create homepage

---
+++ src/app/(frontend)/page.tsx
@@ -0,0 +1,10 @@
+import BeatsList from '@/components/menu/BeatsList'
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-4xl font-semibold">Beat Store</h1>
+      <BeatsList />
+    </>
+  )
+}
