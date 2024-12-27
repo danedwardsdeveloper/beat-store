693cc896b20fde40952b1bacbaca170d725e7e6d
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-27 12:42:24 +0000

    Create metadata.ts

---
+++ src/library/metadata.ts
@@ -0,0 +1,37 @@
+import { Metadata } from 'next'
+
+import { productionBaseURL } from './environment/configuration'
+import { PublicBeatWithAssets } from '@/app/api/types'
+
+export function generateBeatMetadata({ beat }: { beat: PublicBeatWithAssets }): Metadata {
+  const canonical = `${productionBaseURL}/beats/${beat.slug}`
+
+  return {
+    title: beat.metaTitle,
+    description: beat.metaDescription,
+    openGraph: {
+      title: beat.metaTitle,
+      description: beat.metaDescription,
+      type: 'music.song',
+      url: canonical,
+      images: [
+        {
+          url: beat.assetUrls.artworkSocial,
+          width: 1200,
+          height: 630,
+          alt: beat.metaTitle,
+        },
+      ],
+      siteName: 'BeatStore',
+    },
+    alternates: {
+      canonical,
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: beat.metaTitle,
+      description: beat.metaDescription,
+      images: [beat.assetUrls.artworkSocial],
+    },
+  }
+}
