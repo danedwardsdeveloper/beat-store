922db2d624c45609b5bea66e05b803a496dfad98
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-27 12:42:39 +0000

    Move all requireVariable calls

--- src/library/environment/configuration.ts
+++ src/library/environment/configuration.ts
@@ -1,3 +1,5 @@
+import { requireVariable } from './requireVariable'
+
 export const isProduction = process.env.NODE_ENV === 'production'
 export const isDevelopment = process.env.NODE_ENV === 'development'
 
@@ -8,6 +10,20 @@ export const productionBaseURL = `https://${bareCustomDomain}`
 export const developmentBaseURL = 'http://localhost:3000'
 export const dynamicBaseURL = isProduction ? productionBaseURL : developmentBaseURL
 
-const productionAssetsBaseURL = `https://assets.${bareCustomDomain}`
-const developmentAssetsBaseURL = 'https://d1ttulk89fbidl.cloudfront.net'
-export const dynamicAssetsBaseURL = isProduction ? productionAssetsBaseURL : developmentAssetsBaseURL
+export const apiKey = requireVariable('MAILGUN_KEY')
+
+export const jwtSecret = requireVariable('JWT_SECRET')
+
+export const endpointSecret = requireVariable('STRIPE_WEBHOOK_SECRET')
+export const stripeSecretKey = requireVariable('STRIPE_SECRET_KEY')
+export const stripeSecretTestKey = requireVariable('STRIPE_SECRET_TEST_KEY')
+export const useRealMoney = false
+export const stripeDynamicSecretKey = useRealMoney ? stripeSecretKey : stripeSecretTestKey
+
+export const awsAccessKey = requireVariable('AWS_ACCESS_KEY_ID')
+export const awsSecretAccessKey = requireVariable('AWS_SECRET_ACCESS_KEY')
+
+export const cloudfrontURL = requireVariable('CF_URL')
+// export const cfKeyPairId = requireVariable('CF_KEY_PAIR_ID')
+// export const cfPrivateKey = requireVariable('CF_PRIVATE_KEY')
+// export const cfDomain = requireVariable('CF_DOMAIN')
