77bda57adcc970e327f5db1c53257882f1d2c87d
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-27 12:39:08 +0000

    Add Cloudfront remote pattern & rewrites/redirects

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,53 @@
 import type { NextConfig } from 'next'
 
+/* cspell:disable */
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/images/:path*',
+        destination: 'https://d1ttulk89fbidl.cloudfront.net/:path*',
+        basePath: false,
+        locale: false,
+      },
+    ]
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'd1ttulk89fbidl.cloudfront.net',
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.beatstore.co.uk',
+          },
+        ],
+        destination: 'https://beatstore.co.uk/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'beat-store.fly.dev',
+          },
+        ],
+        // ToDO: Change to beatstore.co.uk
+        destination: 'https://beat-store.fly.dev/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default nextConfig
