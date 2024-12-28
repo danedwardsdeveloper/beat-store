59b1c3218a14006b19feefb586b610c354c8a98f
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-28 10:08:32 +0000

    Create homepage

---
+++ src/app/(frontend)/(home-page)/page.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+
+import { defaultMetaDescription } from '@/library/metadata'
+
+import CtaButton from '@/components/CtaButton'
+
+import heroPhoto from './hero.png'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen text-center">
+      <Image src={heroPhoto} alt={defaultMetaDescription} fill className="object-cover -z-10" priority />
+      <h1 className="text-6xl md:max-w-4xl text-zinc-100 mb-4 font-medium drop-shadow-[0_2px_4px_rgba(0,0,0,1)]">{`Sell more beats with a custom high-performance store`}</h1>
+      <CtaButton />
+    </div>
+  )
+}
