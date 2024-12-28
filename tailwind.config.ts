cf2f8ea826102f38f9b712c3060f4e4bbe5ce4b4
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-28 10:09:59 +0000

    Add Inter font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,12 @@
-// import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss'
+import { fontFamily } from 'tailwindcss/defaultTheme'
 
-// export default {
-//   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-//   darkMode: 'selector',
-//   theme: {
-//     extend: {
-//       fontFamily: {
-//         sans: ['var(--font-readex-pro)'],
-//       },
-//     },
-//   },
-// } satisfies Config
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'selector',
+  theme: {
+    extend: {
+      fontFamily: { sans: ['var(--font-inter)', ...fontFamily.sans] },
+    },
+  },
+} satisfies Config
