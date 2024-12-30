a722c9572a4e09681da6a22e4a9e84de09e42c26
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-30 10:58:37 +0000

    Move types

--- src/app/api/types/definitions/safeUser.ts
+++
@@ -1,8 +0,0 @@
-import { ConfirmationStatus, UserRole } from '@prisma/client'
-
-export interface SafeUser {
-  id: string
-  firstName: string
-  role: UserRole
-  confirmationStatus: ConfirmationStatus
-}
--- src/app/api/types/index.ts
+++
@@ -1,6 +0,0 @@
-// Move this to src/types
-export * from './definitions/apiEndpoints'
-export * from './definitions/httpStatus'
-export * from './definitions/responseMessages'
-export * from './definitions/beat'
-export * from './definitions/safeUser'
