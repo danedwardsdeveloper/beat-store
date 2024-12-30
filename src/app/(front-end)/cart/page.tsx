ec2a9c8912a479faa897c76724c5b1747512602a
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-30 10:44:17 +0000

    Create placeholder content

--- src/app/(front-end)/cart/page.tsx
+++ src/app/(front-end)/cart/page.tsx
@@ -1,3 +1,11 @@
+import Heading from '@/components/Heading'
+
 export default function CartPage() {
-  return
+  return (
+    <div className=" py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <Heading title={'Cart'} intro={'Review your items and checkout'} />
+      </div>
+    </div>
+  )
 }
