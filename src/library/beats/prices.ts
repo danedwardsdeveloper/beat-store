8446a0ebdec692d4686b3556dc45f9178480ec11
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-30 10:46:38 +0000

    Create prices.ts

---
+++ src/library/beats/prices.ts
@@ -0,0 +1,30 @@
+import { LicenseType } from '@prisma/client'
+
+import { PublicBeatWithAssets } from '@/types'
+
+const defaultPrices = {
+  basic: 2999,
+  premium: 4999,
+  unlimited: 9999,
+  exclusive: 2999,
+}
+
+function formatPrice(price: number): string {
+  const dollars = price / 100
+  const isWholeNumber = dollars % 1 === 0
+
+  return isWholeNumber
+    ? `$${dollars.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`
+    : `$${dollars.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`
+}
+
+export function getPrice(beat: PublicBeatWithAssets, license: LicenseType) {
+  const customPrices = {
+    basic: beat.customPriceBasic ?? defaultPrices.basic,
+    premium: beat.customPricePremium ?? defaultPrices.premium,
+    unlimited: beat.customPriceUnlimited ?? defaultPrices.unlimited,
+    exclusive: beat.customPriceExclusive ?? defaultPrices.exclusive,
+  }
+  const resolvedPrice = beat.useDefaultPrices ? defaultPrices[license] : customPrices[license]
+  return formatPrice(resolvedPrice)
+}
