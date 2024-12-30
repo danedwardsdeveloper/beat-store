d105d42db5d47ecabd485224b7c08870b5d91a17
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-30 11:07:34 +0000

    Update image and styles

--- src/app/(front-end)/articles/page.tsx
+++ src/app/(front-end)/articles/page.tsx
@@ -1,26 +1,22 @@
 import Image from 'next/image'
 
+import tempImage from '@/app/(front-end)/(home-page)/components/hero/hero.png'
+import Heading from '@/components/Heading'
+
 import { articles } from './data'
 
 export default function ArticlesPage() {
   return (
-    <div className="bg-slate-900 py-24 sm:py-32">
+    <div className=" py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl text-center">
-          <h2 className="text-balance text-4xl font-semibold tracking-tight text-zinc-100 sm:text-5xl">
-            From the blog
-          </h2>
-          <p className="mt-2 text-lg/8 text-zinc-300">
-            Learn how to grow your business with our expert advice.
-          </p>
-        </div>
+        <Heading title={'Articles'} intro={`Learn how to grow your business with our expert advice.`} />
         <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {articles.map(post => (
             <article key={post.id} className="flex flex-col items-start justify-between">
               <div className="relative w-full">
                 <Image
                   alt=""
-                  src={post.imageUrl}
+                  src={tempImage}
                   className="aspect-video w-full rounded-2xl bg-zinc-800 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
                 />
                 <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
