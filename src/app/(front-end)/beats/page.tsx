7150d7a2d4806f004ede31b353855cf6f9135356
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-30 10:44:07 +0000

    Update page.tsx

--- src/app/(front-end)/beats/page.tsx
+++ src/app/(front-end)/beats/page.tsx
@@ -1,3 +1,16 @@
+import { Metadata } from 'next'
+
+import List from '@/components/beats/List'
+
+export const metadata: Metadata = {
+  title: 'Browse beats',
+  description: 'Browse beats on the Beat Store',
+}
+
 export default function AllBeatsPage() {
-  return
+  return (
+    <div className="m-8">
+      <List />
+    </div>
+  )
 }
