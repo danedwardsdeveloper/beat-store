3e50cbcd70b62b77114932e045123658788d0260
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-30 10:58:04 +0000

    Create styles.ts

---
+++ src/app/(front-end)/styles/styles.ts
@@ -0,0 +1,20 @@
+export const buttonClasses = {
+  primary: {
+    base: 'text-white focus-visible:outline-indigo-600  bg-indigo-600  font-semibold transition-colors duration-300 text-base rounded-md px-3 py-2 text-center shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 w-full',
+    active: 'cursor-pointer active:bg-indigo-400 hover:bg-indigo-500',
+    disabled: 'cursor-not-allowed opacity-50',
+  },
+}
+
+export const backgroundClasses = {
+  primary: 'bg-slate-950',
+  secondary: 'bg-slate-800',
+}
+
+export const inputClasses = 'bg-white/80 p-2 mx-4 rounded flex-1 text-zinc-800'
+
+export const menuItemStyles = {
+  base: 'text-xl hover:bg-slate-700 active:bg-slate-600 rounded-lg px-2 py-1 transition-all duration-300',
+  active: 'text-slate-300 bg-slate-800',
+  inactive: 'text-slate-200',
+}
