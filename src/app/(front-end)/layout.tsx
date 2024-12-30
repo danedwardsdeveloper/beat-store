96062d5be8cf48e46585823430a6cdd868a71e75
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-30 10:44:34 +0000

    Add Providers

--- src/app/(front-end)/layout.tsx
+++ src/app/(front-end)/layout.tsx
@@ -3,9 +3,11 @@ import { Inter } from 'next/font/google'
 
 import { defaultMetaDescription } from '@/library/misc/metadata'
 
+import Providers from './(home-page)/components/Providers'
 import Menus from '@/components/menus'
+import NotificationContainer from '@/components/notifications/NotificationContainer'
 
-import './global-styles.tailwind.css'
+import './styles/globals.tailwind.css'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -25,9 +27,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en-GB" className={inter.variable}>
-      <body className="antialiased font-sans bg-black text-zinc-300">
-        <Menus />
-        {children}
+      <body className="antialiased font-sans bg-slate-900 text-zinc-300 min-h-screen">
+        <Providers>
+          <Menus />
+          <main>{children}</main>
+          <NotificationContainer />
+        </Providers>
       </body>
     </html>
   )
