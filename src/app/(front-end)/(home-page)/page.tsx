ed203c6c1c3923c44af7f88e4a9daa7214bfccf5
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-01 10:08:50 +0000

    Use new contact form

--- src/app/(front-end)/(home-page)/page.tsx
+++ src/app/(front-end)/(home-page)/page.tsx
@@ -1,12 +1,14 @@
 import ContactForm from './components/ContactForm'
 import Hero from './components/hero'
 import Pricing from './components/Pricing'
+import ContactFormNew from '@/components/contactForm'
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Pricing />
+      <ContactFormNew />
       <ContactForm />
     </>
   )
