bd9f2fede8bbed4e16d2d680ac97c567c39f05b2
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-03 14:41:19 +0000

    Create marketingCopy.ts

---
+++ src/library/misc/marketingCopy.ts
@@ -0,0 +1,37 @@
+// cspell:disable
+export const marketingCopy = {
+  metaDescription:
+    'Beat Store is a modern high-performance e-commerce website for selling beats. Get yours today.',
+  storyBrand: {
+    short: 'Sell more beats with a custom high-performance store',
+    medium: '',
+    long: '',
+  },
+  contact: {
+    title: 'Get in touch',
+    intro:
+      'Find out how you can sell more beats, keep more of your profits, and have more control over your brand with a custom high-performance beat store.',
+  },
+  pricing: {
+    titles: {
+      cardTitle: 'Lifetime membership',
+      sectionTitle: 'Simple no-tricks pricing',
+    },
+    intros: {
+      cardIntro:
+        'Lorem ipsum dolor sit amet consect etur adipisicing elit. Itaque amet indis perferendis blanditiis repellendus etur quidem assumenda.',
+      sectionIntro: `Distinctio et nulla eum soluta et neque labore quibusdam. Saepe et quasi iusto modi velit ut non voluptas in. Explicabo id ut laborum.`,
+    },
+    whatsIncluded: {
+      title: "What's included",
+      items: [
+        'Private forum access',
+        'Member resources',
+        'Entry to annual conference',
+        'Official member t-shirt',
+      ],
+    },
+    extras: ['Pay once, own it forever', 'Invoices and receipts available for easy company reimbursement'],
+    usdPriceString: '$649',
+  },
+}
