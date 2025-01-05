02e508dda5ba3432ce0ec52107a0cea902d22b00
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-05 11:08:57 +0000

    Update style handling

--- src/components/CtaButton.tsx
+++ src/components/CtaButton.tsx
@@ -1,20 +1,14 @@
 import clsx from 'clsx'
 import Link from 'next/link'
 
-import { buttonClasses } from '@/styles'
-
 const variantData = {
   primary: {
-    // href: 'https://www.upwork.com/',
     href: '/',
     text: 'Get your store',
-    classes: clsx(buttonClasses.base, buttonClasses.primary.base, buttonClasses.primary.active),
   },
   secondary: {
-    // href: '/articles/learn-more',
     href: '/',
     text: 'Learn more',
-    classes: clsx(buttonClasses.base, buttonClasses.secondary.base, buttonClasses.secondary.active),
   },
 } as const
 
@@ -25,10 +19,18 @@ export default function CtaButton({
   variant: 'primary' | 'secondary'
   classes?: string
 }) {
+  const isPrimary = variant === 'primary'
   const data = variantData[variant]
 
   return (
-    <Link href={data.href} className={clsx(data.classes, 'block w-full', classes)}>
+    <Link
+      href={data.href}
+      className={clsx(
+        isPrimary ? 'button-primary' : 'button-secondary', //
+        'block w-full',
+        classes,
+      )}
+    >
       {data.text}
     </Link>
   )
