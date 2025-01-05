a4b4f973022b08a58800c7f7037c7c0dab450a7d
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-05 11:09:34 +0000

    Change bareFlyDomain to fullFlyDomain

--- src/library/environment/publicVariables.ts
+++ src/library/environment/publicVariables.ts
@@ -1,7 +1,7 @@
 export const isProduction = process.env.NODE_ENV === 'production'
 export const isDevelopment = process.env.NODE_ENV === 'development'
 
-export const bareFlyDomain = 'beat-store.fly.dev'
+export const fullFlyDomain = 'https://beat-store.fly.dev'
 export const bareCustomDomain = 'beatstore.co.uk'
 
 export const productionBaseURL = `https://${bareCustomDomain}`
