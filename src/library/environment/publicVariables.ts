7547b11d93cd275bfb4ff88c520c004f90695d1d
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-07 10:00:32 +0000

    Update bareDomain variable

--- src/library/environment/publicVariables.ts
+++ src/library/environment/publicVariables.ts
@@ -1,9 +1,8 @@
 export const isProduction = process.env.NODE_ENV === 'production'
 export const isDevelopment = process.env.NODE_ENV === 'development'
 
-export const fullFlyDomain = 'https://beat-store.fly.dev'
-export const bareCustomDomain = 'beatstore.co.uk'
+export const bareDomain = 'beat-store.fly.dev'
 
-export const productionBaseURL = `https://${bareCustomDomain}`
+export const productionBaseURL = `https://${bareDomain}`
 export const developmentBaseURL = 'http://localhost:3000'
 export const dynamicBaseURL = isProduction ? productionBaseURL : developmentBaseURL
