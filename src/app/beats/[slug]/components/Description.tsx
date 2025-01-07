67f18c4e717cfa6c90fdb95a9c501ab579b42ef5
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-07 20:24:27 +0000

    Add spacing

--- src/app/beats/[slug]/components/Description.tsx
+++ src/app/beats/[slug]/components/Description.tsx
@@ -4,13 +4,14 @@ export default function Description({ beat }: { beat: PublicBeatWithAssets }) {
   return (
     <>
       <h2 className="text-base/7 font-semibold text-indigo-400">Beat Store</h2>
-      <p className="mt-2 max-w-lg text-pretty text-4xl font-semibold tracking-tight text-white sm:text-5xl">
+      <p className="mt-2 mb-16 max-w-lg text-pretty text-4xl font-semibold tracking-tight text-white sm:text-5xl">
         {beat.title}
       </p>
-      <h1></h1>
-      {beat.description.map((paragraph, index) => (
-        <p key={index}>{paragraph}</p>
-      ))}
+      <div className="flex flex-col space-y-2 mb-8">
+        {beat.description.map((paragraph, index) => (
+          <p key={index}>{paragraph}</p>
+        ))}
+      </div>
     </>
   )
 }
