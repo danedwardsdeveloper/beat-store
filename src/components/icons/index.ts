cb06b7572865148eacc958d20fd98b6d3c34ebfa
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-09 15:58:14 +0000

    Update import path

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,4 +1,4 @@
-export * from './definitions/MenuIcons'
+export * from './definitions/ThreeBarsIcon'
 export * from './PlayPauseIcon'
 export * from './definitions/SvgContainer'
 export * from './definitions/FooterIcons'
