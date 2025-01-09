e586404a897dcc109eb1f38332e8300709bf0f0d
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-09 15:56:08 +0000

    Create ThreeBarsIcon.tsx

---
+++ src/components/icons/definitions/ThreeBarsIcon.tsx
@@ -0,0 +1,9 @@
+import { SvgContainer } from './SvgContainer'
+
+export function ThreeBarsIcon() {
+  return (
+    <SvgContainer size="size-8">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+    </SvgContainer>
+  )
+}
