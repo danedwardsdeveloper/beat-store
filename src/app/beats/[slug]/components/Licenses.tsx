8ddfa643ea04add557b3aadaaf704f941af773e9
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-10 16:45:06 +0000

    Add AddToCartButton

--- src/app/beats/[slug]/components/Licenses.tsx
+++ src/app/beats/[slug]/components/Licenses.tsx
@@ -9,6 +9,8 @@ import { defaultPrices } from '@/library/beats/prices'
 import { capitalizeFirstLetter } from '@/library/formatting/capitaliseFirstLetter'
 import formatPrice from '@/library/formatting/formatPrice'
 
+import AddToCartButton from '../../components/AddToCartButton'
+
 import { PublicBeatWithAssets } from '@/types'
 
 type LicenseOptions = {
@@ -43,41 +45,58 @@ const licenseOptions: LicenseOptions = {
 }
 
 export default function Licenses({ beat }: { beat: PublicBeatWithAssets }) {
-  const [selectedLicenseOption, setSelectedLicenseOption] = useState(licenseOptions['premium'])
+  const [selectedLicenseOption, setSelectedLicenseOption] = useState<LicenseType>(LicenseType.premium)
 
   return (
-    <fieldset>
-      <legend className="text-sm/6 font-semibold text-zinc-200">Select a license</legend>
-      <RadioGroup
-        value={selectedLicenseOption}
-        onChange={setSelectedLicenseOption}
-        className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-3 sm:gap-x-4"
-      >
-        {Object.entries(licenseOptions).map(([type, option]) => (
-          <Radio
-            key={option.id}
-            value={option}
-            aria-label={type}
-            className="group relative flex cursor-pointer rounded-lg border border-zinc-500 bg-white/5 p-4 shadow-sm focus:outline-none data-[focus]:border-indigo-600 data-[focus]:ring-2 data-[focus]:ring-indigo-600 transition-colors duration 300"
+    <>
+      <div className="col-span-9 grid grid-cols-1 gap-4">
+        <fieldset>
+          <legend className="text-sm/6 font-semibold text-zinc-200">Select a license</legend>
+          <RadioGroup
+            value={licenseOptions[selectedLicenseOption]}
+            onChange={option => {
+              const type = Object.entries(licenseOptions).find(
+                ([_, opt]) => opt === option,
+              )?.[0] as LicenseType
+              setSelectedLicenseOption(type)
+            }}
+            className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-3 sm:gap-x-4"
           >
-            <span className="flex flex-1">
-              <span className="flex flex-col">
-                <span className="block text-sm font-medium text-zinc-100">{capitalizeFirstLetter(type)}</span>
-                <span className="mt-1 flex items-center text-sm text-zinc-300">{option.description}</span>
-                <span className="mt-6 text-sm font-medium text-zinc-100">{formatPrice(option.price)}</span>
-              </span>
-            </span>
-            <CheckCircleIcon
-              aria-hidden="true"
-              className="size-5 text-indigo-600 group-[&:not([data-checked])]:invisible"
-            />
-            <span
-              aria-hidden="true"
-              className="pointer-events-none absolute -inset-px rounded-lg border-2 border-transparent group-data-[focus]:border group-data-[checked]:border-indigo-600"
-            />
-          </Radio>
-        ))}
-      </RadioGroup>
-    </fieldset>
+            {Object.entries(licenseOptions).map(([type, option]) => (
+              <Radio
+                key={option.id}
+                value={option}
+                aria-label={type}
+                className="group relative flex cursor-pointer rounded-lg border border-zinc-500 bg-white/5 p-4 shadow-sm focus:outline-none data-[focus]:border-indigo-600 data-[focus]:ring-2 data-[focus]:ring-indigo-600 transition-colors duration 300"
+              >
+                <span className="flex flex-1">
+                  <span className="flex flex-col">
+                    <span className="block text-sm font-medium text-zinc-100">
+                      {capitalizeFirstLetter(type)}
+                    </span>
+                    <span className="mt-1 flex items-center text-sm text-zinc-300">{option.description}</span>
+                    <span className="mt-6 text-sm font-medium text-zinc-100">
+                      {formatPrice(option.price)}
+                    </span>
+                  </span>
+                </span>
+                <CheckCircleIcon
+                  aria-hidden="true"
+                  className="size-5 text-indigo-600 group-[&:not([data-checked])]:invisible"
+                />
+                <span
+                  aria-hidden="true"
+                  className="pointer-events-none absolute -inset-px rounded-lg border-2 border-transparent group-data-[focus]:border group-data-[checked]:border-indigo-600"
+                />
+              </Radio>
+            ))}
+          </RadioGroup>
+        </fieldset>
+      </div>
+
+      <div className="mt-16 max-w-sm mx-auto">
+        <AddToCartButton id={beat.id} license={selectedLicenseOption} />
+      </div>
+    </>
   )
 }
