0084da9b7f345d6a0c6fe8252c67c00e0077cb10
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-10 16:44:31 +0000

    Implement useLocalStorage

--- src/app/beats/components/AddToCartButton.tsx
+++ src/app/beats/components/AddToCartButton.tsx
@@ -1,9 +1,31 @@
 'use client'
 
+import { LocalStorageCartItem, useLocalStorage } from '@/providers/localStorage'
+import { useNotifications } from '@/providers/notifications'
 import { useUi } from '@/providers/ui'
 
-export default function AddToCartButton() {
-  const { incrementCartCount } = useUi()
+export default function AddToCartButton({ id, license }: LocalStorageCartItem) {
+  const { incrementCartCount, uiSignedIn } = useUi()
+  const { addToCart } = useLocalStorage()
+  const { createNotification } = useNotifications()
 
-  return <button onClick={incrementCartCount} className="button-primary w-full">{`Add to cart`}</button>
+  function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault()
+    incrementCartCount()
+    if (uiSignedIn) {
+      return null
+    } else {
+      addToCart({
+        id,
+        license,
+      })
+    }
+    createNotification({
+      title: '',
+      message: '',
+      level: 'success',
+    })
+  }
+
+  return <button onClick={handleClick} className="button-primary w-full">{`Add to cart`}</button>
 }
